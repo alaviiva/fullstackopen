343289b4a74a2064d60d6e4518a4569ce11c65ed
Sampo Vartiala <sampo.vartiala@gmail.com>
2022-01-08 14:50:50 +0200

    4.17

--- osa4/controllers/blogs.js
+++ osa4/controllers/blogs.js
@@ -1,15 +1,21 @@
 const blogsRouter = require('express').Router()
 const Blog = require('../models/blog')
+const User = require('../models/user')
 
 blogsRouter.get('/', async (request, response) => {
-  const blogs = await Blog.find({})
+  const blogs = await Blog.find({}).populate('user', {username: 1, name: 1})
   response.json(blogs)
 })
 
 blogsRouter.post('/', async (request, response, next) => {
   const blog = new Blog(request.body)
+  const users = await User.find({})
+  const user = users[0]
+  blog.user = user._id
 
   const newblog = await blog.save()
+  await User.findByIdAndUpdate(user._id, {blogs: user.blogs.concat(newblog._id)})
+
   response.status(201).json(newblog)
 })
 
