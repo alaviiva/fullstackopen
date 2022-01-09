ae64ebd2f9c7d9f37b3317943d1922e684c5048c
Sampo Vartiala <sampo.vartiala@gmail.com>
2022-01-10 00:48:39 +0200

    5.18

--- osa5/bloglist-frontend/cypress/integration/bloglist.spec.js
+++ osa5/bloglist-frontend/cypress/integration/bloglist.spec.js
@@ -3,6 +3,12 @@
 describe('Blog app', function() {
   beforeEach(function() {
     cy.request('POST', 'http://localhost:3003/api/testing/reset')
+    const user = {
+      name: 'Ville Kebab',
+      username: 'ville',
+      password: 'passu'
+    }
+    cy.request('POST', 'http://localhost:3003/api/users/', user)
     cy.visit('http://localhost:3000')
   })
 
@@ -11,5 +17,23 @@ describe('Blog app', function() {
     cy.get('#username')
     cy.get('#password')
     cy.get('#login-button')
+  })
+
+
+  describe('Login',function() {
+    it('succeeds with correct credentials', function() {
+      cy.get('#username').type('ville')
+      cy.get('#password').type('passu')
+      cy.get('#login-button').click()
+      cy.contains('blogs')
+      cy.contains('ville logged in')
+    })
+
+    it('fails with wrong credentials', function() {
+        cy.get('#username').type('kala')
+        cy.get('#password').type('kala')
+        cy.get('#login-button').click()
+        cy.contains('wrong credentials')
+    })
   })
 })
