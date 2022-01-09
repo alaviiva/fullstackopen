e313184604e9cda11f212972925f04b3646ee36a
Sampo Vartiala <sampo.vartiala@gmail.com>
2022-01-09 15:37:46 +0200

    5.7

--- osa5/bloglist-frontend/src/components/Blog.js
+++ osa5/bloglist-frontend/src/components/Blog.js
@@ -1,10 +1,36 @@
-import React from 'react'
+import React, {useState} from 'react'
 
-const Blog = ({blog}) => (
-  <div>
-    {blog.title} - {blog.author}
-  </div>
-)
+const Blog = ({blog}) => {
+  const [showInfo, setShowInfo] = useState(false)
+
+  const toggleInfo = () => setShowInfo(!showInfo)
+
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: 'solid',
+    borderWidth: 1,
+    marginBottom: 5
+  }
+
+  if (!showInfo)
+    return (
+      <div style={blogStyle}>
+        {blog.title} - {blog.author}
+        <button onClick={toggleInfo} >view</button>
+      </div>
+    )
+
+  return (
+    <div style={blogStyle}>
+      {blog.title}
+      <button onClick={toggleInfo} >hide</button>
+      <div>{blog.url}</div>
+      <div>likes: {blog.likes}<button onClick={() => console.log(blog)}>like</button></div>
+      <div>{blog.author}</div>
+    </div>
+  )
+}
 
 const BlogList = ({blogs}) => {
   return (
