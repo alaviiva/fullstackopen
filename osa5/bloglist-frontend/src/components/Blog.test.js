6e1ca3df6daee039289c36721a72395f3da43faa
Sampo Vartiala <sampo.vartiala@gmail.com>
2022-01-09 22:39:19 +0200

    5.14

--- osa5/bloglist-frontend/src/components/Blog.test.js
+++ osa5/bloglist-frontend/src/components/Blog.test.js
@@ -1,28 +1,49 @@
 import React from 'react'
 import '@testing-library/jest-dom/extend-expect'
-import { render } from '@testing-library/react'
+import { render, fireEvent } from '@testing-library/react'
+import { prettyDOM } from '@testing-library/dom'
 import BlogList from './Blog'
 
-test('renders content', () => {
-  const blog = [{
-    title: 'Blog testing',
-    author: 'Timppa',
-    url: 'kissa.kala/test',
-    likes: 3,
-    id: 'abc',
-    user: {username: 'test'}
-  }]
 
-  const user = {username: 'test'}
-  const k = () => 'k'
-  const component = render(
-    <BlogList blogs={blog} update={k} user={user} />
-  )
+describe('Blog list', () => {
+  let component
 
-  expect(component.container).toHaveTextContent(
-    'Blog testing - Timppa'
-  )
-  expect(component.container).not.toHaveTextContent(
-    'kissa.kala/test'
-  )
+  beforeEach(() => {
+    const blog = [{
+      title: 'Blog testing',
+      author: 'Timppa',
+      url: 'kissa.kala/test',
+      likes: 3,
+      id: 'abc',
+      user: {username: 'test'}
+    }]
+
+    const user = {username: 'test'}
+
+    const mockHandler = jest.fn()
+
+    component = render(
+      <BlogList blogs={blog} update={mockHandler} user={user} />
+    )
+  })
+
+  test('renders content', () => {
+    expect(component.container).toHaveTextContent(
+      'Blog testing - Timppa'
+    )
+    expect(component.container).not.toHaveTextContent(
+      'kissa.kala/test'
+    )
+  })
+
+  test('clicking view-button shows more info', () => {
+    const button = component.getByText('view')
+    fireEvent.click(button)
+    expect(component.container).toHaveTextContent(
+      'kissa.kala/test'
+    )
+    expect(component.container).toHaveTextContent(
+      'likes: 3'
+    )
+  })
 })
