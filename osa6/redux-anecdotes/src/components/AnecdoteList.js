7eb27754f6bc2ab844c37489b52a938388603245
Sampo Vartiala <sampo.vartiala@gmail.com>
2022-01-13 19:27:54 +0200

    6.10-6.11

--- osa6/redux-anecdotes/src/components/AnecdoteList.js
+++ osa6/redux-anecdotes/src/components/AnecdoteList.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import { vote } from '../reducers/anecdoteReducer'
+import { setNotification } from '../reducers/notificationReducer'
 
 const Anecdote = ({ anecdote, handleClick }) => {
   return(
@@ -10,19 +11,25 @@ const Anecdote = ({ anecdote, handleClick }) => {
       </div>
       <div>
         has {anecdote.votes}
-        <button onClick={() => handleClick(anecdote.id)}>vote</button>
+        <button onClick={() => handleClick(anecdote)}>vote</button>
       </div>
     </div>
   )
 }
 
 const AnecdoteList = () => {
-  const anecdotes = useSelector(state => state.sort((a, b) => b.votes - a.votes))
+  const anecdotes = useSelector(state => state.anecdotes.sort(
+    (a, b) => b.votes - a.votes)
+  )
   const dispatch = useDispatch()
 
-  const handleVote = (id) => {
-    console.log('vote', id)
-    dispatch(vote(id))
+  const handleVote = (anecdote) => {
+    console.log('vote', anecdote.id)
+    dispatch(vote(anecdote.id))
+    dispatch(setNotification(`you voted '${anecdote.content}'`))
+    setTimeout(() => {
+      dispatch(setNotification(''))
+    }, 5000)
   }
 
   return(
